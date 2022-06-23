51741f9e9e9c0241bd69809a8849777767a85207
DrummyG <balestrinodavide@gmail.com>
2022-06-23 16:38:29 +0200

    lavoro base completato plus bonus

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return view('welcome', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+      
+        $newCategory = Category::create($data);
+
+        $newCategory->save();
+        
+        return redirect()->route('categories.show', $newCategory->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        if($category){
+            return view('categories.show', compact('category'));
+        }
+        abort(404);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        return view('categories.edit',compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $data = $request->all();
+
+        $category->update($data);
+
+        return redirect()->route('categories.show', $category->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+        return redirect()->route('home');
+    }
+}
---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,25 @@
+@extends('parts.body')
+
+@section('content')
+<h1>La tua base operativa per i posts</h1>
+
+<div class="container">
+    <div class="row">
+        @foreach ($posts as $post)
+            <div class="col-3">
+                <a href="{{route('posts.show', $post->id)}}">
+                    <h3>{{$post->title}}</h3>
+                    <p>{{$post->description}}</p>
+                </a>
+                <form action='{{ route('posts.destroy', $post->id) }}' method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger m-2">Elimina</button>
+                </form>
+            </div>
+        @endforeach
+    </div>
+    <button><a href="{{route('posts.create')}}">Crea Nuovo Post</a></button>
+    <button><a href="{{route('categories.create')}}">Crea Nuova Categoria</a></button>
+</div>
+@endsection
